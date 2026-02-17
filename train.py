631a111865b7e6403ae09afbc98e01a6e62f7969
John Nguyen <nguyenj1863@gmail.com>
2026-02-17 13:07:24 -0500

    feat: add ESC50Dataset class with init, len, and getitem methods for audio data loading

--- train.py
+++ train.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+import pandas as pd
+
 import modal
+import torch
+from torch.utils.data import Dataset
+import torchaudio
 
 app = modal.App("audio-cnn")
 
@@ -26,7 +32,41 @@ volume = modal.Volume.from_name("esc50-data", create_if_missing=True)
 # Volume for storing training outputs
 modal_volume = modal.Volume.from_name("esc50-data", create_if_missing=True)
 
+class ESC50Dataset(Dataset):
+    def __init__(self, data_dir, metadata_file, split="train", transform=None):
+        super().__init__()
+        self.data_dir = Path(data_dir)
+        self.metadata = pd.read_csv(metadata_file)
+        self.split = split
+        self.transform = transform
 
+        if split == 'train':
+            self.metadata = self.metadata[self.metadata['fold'] != 5]
+        else:
+            self.metadata = self.metadata[self.metadata['fold'] == 5]
+        
+        self.classes = sorted(self.metadata['category'].unique())
+        self.class_to_idx = {cls: idx for idx, cls in enumerate(self.classes)}
+        self.metadata['label'] = self.metadata['category'].map(self.class_to_idx)
+    
+    def __len__(self):
+        return len(self.metadata)
+
+    def __getitem__(self, idx):
+        row = self.metadata.iloc[idx]
+        audio_path = self.data_dir / "audio" / row['filename']
+        
+        waveform, sample_rate = torchaudio.load(audio_path)
+        
+        if waveform.shape[0] > 1:
+            waveform = torch.mean(waveform, dim=0, keepdim=True)
+
+        if self.transform:
+            spectogram = self.transform(waveform)
+        else:
+            spectogram = waveform
+        
+        return spectogram, row['label']
 
 @app.function(
     image=image,

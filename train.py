1fc1fa9ffcb1562c3038910be46466dd3c43f5fc
John Nguyen <nguyenj1863@gmail.com>
2026-02-17 12:36:51 -0500

    fix: add comma between last two run commands for image, as well as rename directory path to copy esc50 unzipped data

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ image = (modal.Image.debian_slim()
              "cd /tmp && wget https://github.com/karolpiczak/ESC-50/archive/master.zip -O esc50.zip",
              "cd /tmp && unzip esc50.zip",
              "mkdir -p /opt/esc50-data",
-             "cp -r /tmp/ESC-50-master/audio/* /opt/esc50-data/"
+             "cp -r /tmp/ESC-50-master/* /opt/esc50-data/",
              "rm -rf /tmp/esc50.zip /tmp/ESC-50-master"
          ])
          .add_local_python_source("model"))

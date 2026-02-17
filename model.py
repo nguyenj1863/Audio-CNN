a0dd7f48a7dbddde4d8fc44367e38f066c9e534b
John Nguyen <nguyenj1863@gmail.com>
2026-02-17 11:44:41 -0500

    feat: add neural network architecture

--- model.py
+++ model.py
@@ -32,3 +32,40 @@ class ResidualBlock(nn.Module):
         out = torch.relu(out_add)
 
         return out
+    
+class AudioCNN(nn.Module):
+    def __init__(self, num_classes=50):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        self.layer1 = nn.ModuleList([ResidualBlock(in_channels=64, out_channels=64) for i in range(3)])
+        self.layer2 = nn.ModuleList(
+            [ResidualBlock(in_channels=64 if i == 0 else 128, out_channels=128) for i in range(4)])
+        self.layer3 = nn.ModuleList(
+            [ResidualBlock(in_channels=128 if i == 0 else 256, out_channels=256) for i in range(6)])
+        self.layer4 = nn.ModuleList(
+            [ResidualBlock(in_channels=256 if i == 0 else 512, out_channels=512) for i in range(3)])
+        
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc = nn.Linear(in_features=512, out_features=num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        for block in self.layer1:
+            x = block(x)
+        for block in self.layer2:
+            x = block(x)
+        for block in self.layer3:
+            x = block(x)
+        for block in self.layer4:
+            x = block(x)
+        x = self.avgpool(x)
+        x =x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x

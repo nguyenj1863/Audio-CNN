1d4adf902e449799b97bb46cfa3f5ebc29595b52
John Nguyen <nguyenj1863@gmail.com>
2026-02-17 12:23:21 -0500

    feat: setup debian modal container to get audio file from esc50 dataset

--- train.py
+++ train.py
@@ -2,6 +2,18 @@ import modal
 
 app = modal.App("audio-cnn")
 
+image = (modal.Image.debian_slim()
+         .pip_install_from_requirements("requirements.txt")
+         .apt_install(["wget", "unzip", "ffmpeg", "libsndfile1"])
+         .run_commands([
+             "cd /tmp && wget https://github.com/karolpiczak/ESC-50/archive/master.zip -O esc50.zip",
+             "cd /tmp && unzip esc50.zip",
+             "mkdir -p /opt/esc50-data",
+             "cp -r /tmp/ESC-50-master/audio/* /opt/esc50-data/"
+             "rm -rf /tmp/esc50.zip /tmp/ESC-50-master"
+         ])
+         .add_local_python_source("model"))
+
 @app.function()
 def square(x):
     print("This code is running on a remote worker!")
